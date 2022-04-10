984e54dcf81628da8ccac6326e96352877360369
smartdev0218 <antonio.krijan@gmail.com>
2022-04-10 18:19:12 +0200

    coin flip contract

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use borsh::{ BorshDeserialize, BorshSerialize };
+use near_sdk::{
+    env, near_bindgen, AccountId, Balance, Promise,
+    collections::{ UnorderedMap },
+    json_types::{ U128 },
+};
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+const ONE_NEAR:u128 = 1_000_000_000_000_000_000_000_000;
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Trust {
+    pub owner_id: AccountId,
+    pub balances: UnorderedMap<AccountId, Balance>,
+}
+
+impl Default for Trust {
+    fn default() -> Self {
+        panic!("should be initialized before usage")
+    }
+}
+
+#[near_bindgen]
+impl Trust {
+    #[init]
+    pub fn new(owner_id: AccountId) -> Self {
+        assert!(env::is_valid_account_id(owner_id.as_bytes()), "Owner's account ID is invalid.");
+        assert!(!env::state_exists(), "Already initialized");
+        Self {
+            owner_id,
+            balances: UnorderedMap::new(b"balances".to_vec()),
+        }
+    }
+
+    #[payable]
+    pub fn deposit(&mut self) {
+        let deposit = env::attached_deposit();
+        let account_id = env::signer_account_id();
+        let mut balance = self.balances.get(&account_id).unwrap_or(0);
+        balance += deposit;
+        self.balances.insert(&account_id, &balance);
+    }
+
+    pub fn play(&mut self, opt: u8, opt_bal: u128) -> u8 {
+        let account_id = env::signer_account_id();
+        let mut credits = self.balances.get(&account_id).unwrap_or(0);
+        assert!(credits > 0, "no credits to play");
+
+        let rng:u8 = *env::random_seed().get(0).unwrap() % 2;
+
+        let acc_balance = 0;
+        self.balances.insert(&account_id, &acc_balance);
+        if rng == opt {
+            credits += opt_bal * ONE_NEAR * 965 / 100000;
+            Promise::new(account_id).transfer(credits);
+        }
+        return rng;
+    }
+
+    pub fn get_balance(&self, account_id: AccountId) -> U128 {
+        self.balances.get(&account_id).unwrap_or(0).into()
+    }
+}
